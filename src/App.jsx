47770926bbd3d79503c30f4f685e4c5727dbd8d9
adamcesco <acescobedo@smu.edu>
2023-10-02 16:27:53 -0500

    setup eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import React from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      greetMsg: "",
-      name: ""
+      greetMsg: '',
+      name: '',
     };
 
     this.greet = this.greet.bind(this);
@@ -16,25 +16,26 @@ class App extends React.Component {
 
   greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    const name = this.state.name;
-    invoke("greet", {name}).then((greetMsg) => {
-      this.setState({greetMsg});
+    const { name } = this.state;
+    invoke('greet', { name }).then((greetMsg) => {
+      this.setState({ greetMsg });
     });
   }
 
   render() {
+    const { greetMsg } = this.state;
     return (
       <div className="container">
         <h1>Welcome to Tauri!</h1>
 
         <div className="row">
-          <a href="https://vitejs.dev" target="_blank">
+          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
             <img src="/vite.svg" className="logo vite" alt="Vite logo" />
           </a>
-          <a href="https://tauri.app" target="_blank">
+          <a href="https://tauri.app" target="_blank" rel="noreferrer">
             <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
           </a>
-          <a href="https://reactjs.org" target="_blank">
+          <a href="https://reactjs.org" target="_blank" rel="noreferrer">
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
@@ -50,13 +51,13 @@ class App extends React.Component {
         >
           <input
             id="greet-input"
-            onChange={(e) => this.setState({name: e.target.value})}
+            onChange={(e) => this.setState({ name: e.target.value })}
             placeholder="Enter a name..."
           />
           <button type="submit">Greet</button>
         </form>
 
-        <p>{this.state.greetMsg}</p>
+        <p>{greetMsg}</p>
       </div>
     );
   }

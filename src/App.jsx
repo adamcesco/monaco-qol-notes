63a5382cf4bf9356c27b2a68fc3ab246b1ba0758
adamcesco <acescobedo@smu.edu>
2023-10-02 17:32:29 -0500

    setup basic monaco editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,44 @@
 import React from 'react';
-import { invoke } from '@tauri-apps/api/tauri';
-import reactLogo from './assets/react.svg';
+import Editor from '@monaco-editor/react';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      greetMsg: '',
-      name: '',
+      text: '',
     };
-
-    this.greet = this.greet.bind(this);
-  }
-
-  greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    const { name } = this.state;
-    invoke('greet', { name }).then((greetMsg) => {
-      this.setState({ greetMsg });
-    });
   }
 
   render() {
-    const { greetMsg } = this.state;
+    const { text } = this.state;
+    const options = {
+      cursorStyle: 'line',
+      defaultValue: '',
+      folding: false,
+      glyphMargin: false,
+      language: 'markdown',
+      lightbulb: {
+        enabled: false,
+      },
+      lineDecorationsWidth: 0,
+      lineNumbers: 'off',
+      lineNumbersMinChars: 0,
+      quickSuggestions: false,
+      readOnly: false,
+      roundedSelection: false,
+      theme: 'vs',
+      trimAutoWhitespace: false,
+      unicodeHighlight: true,
+      wrappingIndent: 'same',
+    };
+
     return (
-      <div className="container">
-        <h1>Welcome to Tauri!</h1>
-
-        <div className="row">
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-          </a>
-          <a href="https://tauri.app" target="_blank" rel="noreferrer">
-            <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-
-        <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-        <form
-          className="row"
-          onSubmit={(e) => {
-            e.preventDefault();
-            this.greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => this.setState({ name: e.target.value })}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-
-        <p>{greetMsg}</p>
-      </div>
+      <Editor
+        height="90vh"
+        value={text}
+        options={options}
+      />
     );
   }
 }

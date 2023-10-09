eaa44dbbbf8b58c097566967e5c8b0d89c9faec2
adamcesco <acescobedo@smu.edu>
2023-10-09 17:20:32 -0500

    fixed opening and saving logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { save, open, message } from '@tauri-apps/api/dialog';
-import { listen } from '@tauri-apps/api/event';
+import { emit, listen } from '@tauri-apps/api/event';
 import { invoke } from '@tauri-apps/api/tauri';
 import MonacoEditorWrapper from './MonacoEditorWrapper';
 // add global short cuts for monaco editor
+
+window.onkeydown = (e) => {
+  if (e.ctrlKey && e.code === 'KeyP') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.code === 'KeyO') {
+    e.preventDefault();
+    emit('file-menu-event', {
+      payload: 'open',
+    });
+  }
+  if (e.ctrlKey && e.code === 'KeyS') {
+    e.preventDefault();
+    emit('file-menu-event', {
+      payload: 'save',
+    });
+  }
+  if (e.ctrlKey && e.shiftKey && e.code === 'KeyS') {
+    e.preventDefault();
+    emit('file-menu-event', {
+      payload: 'save-as',
+    });
+  }
+};
 
 class App extends React.Component {
   constructor() {
@@ -24,13 +48,12 @@ class App extends React.Component {
         }
 
         case 'save': {
-          this.onSave();
+          this.onSave(this.filePathRef.current);
           break;
         }
 
         case 'save-as': {
-          this.filePathRef.current = null;
-          this.onSave();
+          this.onSave(null);
           break;
         }
 
@@ -45,18 +68,19 @@ class App extends React.Component {
     this.unlistenFileMenuRef.current.then((remove) => remove());
   }
 
-  async onSave() {
-    if (this.filePathRef.current === null) {
-      this.filePathRef.current = await save({
-        defaultPath: 'untitled.md',
-        filters: [{
-          name: 'md',
-          extensions: ['md', 'txt'],
-        }],
-      });
-    }
-    if (this.filePathRef.current !== null) {
+  async onSave(fp) {
+    const filePath = fp || await save({
+      defaultPath: 'untitled.md',
+      filters: [{
+        name: 'md',
+        extensions: ['md', 'txt'],
+      }],
+    });
+    if (filePath !== null) {
       invoke('write_file', { invokePath: this.filePathRef.current, invokeContent: this.editorRef.editor.getValue() })
+        .then(() => {
+          this.filePathRef.current = filePath;
+        })
         .catch(async (error) => {
           await message(`An error occured while writing to the following filepath: ${this.filePathRef.current}.\n\nThe details of the error are: ${error}`, { title: 'File Writing Error', type: 'error' });
         });
